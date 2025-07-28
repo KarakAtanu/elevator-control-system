af7405581c35b5123511a852c6c716e101ac1cfd
atanukarak2011 <atanukarak2011@gmail.com>
2025-07-28 23:46:31 +0530

    Add appsettings.json and configure required values for elevator

--- ElevatorControlSystem.Application/Program.cs
+++ ElevatorControlSystem.Application/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ElevatorControlSystem.Application.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+var configuration = new ConfigurationBuilder()
+	.SetBasePath(Directory.GetCurrentDirectory())
+	.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+	.Build();
+
+services.Configure<ElevatorSettings>(configuration.GetSection(nameof(ElevatorSettings)));
+
+Console.ReadLine();
---
+++ ElevatorControlSystem.Application/Settings/ElevatorSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ElevatorControlSystem.Application.Settings
+{
+	public class ElevatorSettings
+	{
+		public int MinFloor { get; set; }
+		public int MaxFloor { get; set; }
+		public int ElevatorCount { get; set; }
+	}
+}
