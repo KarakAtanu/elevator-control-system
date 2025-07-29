76bf2ac105af250c84beffb02c1ac61cff273298
atanukarak2011 <atanukarak2011@gmail.com>
2025-07-29 12:36:32 +0530

    Removed ElevatorFactory - its a overkill

--- ElevatorControlSystem.Service/Interfaces/IElevatorFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using ElevatorControlSystem.Domain.Models;
-
-namespace ElevatorControlSystem.Service.Interfaces
-{
-	public interface IElevatorFactory
-	{
-		Elevator Create(int id, int minFloor, int maxFloor);
-	}
-}
